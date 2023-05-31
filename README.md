bcee6ad0e304cfcf62f12a128779cd861be5191e
Matthieu Mota <matthieumota@gmail.com>
2023-05-31 10:22:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Découvrir PHP
+
+Ce dépôt Github est la source pour pouvoir travailler sur [cette introduction à PHP](https://innate-gerbil-c60.notion.site/PHP-et-Introduction-Symfony-aeffb8d5c4f04db2b2978b589c08b444).
---
+++ step1/data.php
@@ -0,0 +1,9 @@
+<?php
+
+$title = 'Quae dolor itaque natus reiciendis ad quae.';
+$price = 38; // sans taxes
+$discount = 19;
+$isbn = '8248827583739'; // ISBN 13 ou 10 non formaté 2-7654-1005-4 ou 2-765412-005123
+$author = 'Denise-Sabine Bernard';
+$publishedAt = '2014-08-18';
+$image = 'uploads/06.jpg';
